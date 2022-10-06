448ffb248be153bcf0fc150e9c13f87e30741225
mausamik <mausamikarogalo1@gmail.com>
2022-10-06 20:49:31 +0530

    commit

--- Javascript_basics/input.js
+++ Javascript_basics/input.js
@@ -1,4 +1,8 @@
 // HOW TO TAKE INPUT IN JS 
+//prompt will not work in vscode 
+// install using npm install prompt-sync 
+// and always import this 
+const prompt=require("prompt-sync")({sigint:true}); 
 
 var age = prompt("Enter age : ")
 var Name = prompt("Enter name : ")
---
+++ Javascript_basics/loops.js
@@ -0,0 +1,32 @@
+/*
+
+Types of Loops 
+    for
+    while
+    do-while
+    for-in 
+*/
+
+// for 
+console.log("for loop")
+for(i = 1 ; i < 5 ; i++)
+{
+    console.log(i + " " )
+}
+
+
+console.log("while loop")
+var j = 1 
+while(j < 5 ){
+    console.log(j+ " ");
+    j++;
+}
+
+
+console.log(" do while loop")
+var k = 1 
+do{
+    console.log(k + " ");
+    k++;
+
+}while(k< 5);
