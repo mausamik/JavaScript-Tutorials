926f01bffcfc0bc062d1cb6d5a065bfbe94962fd
mausamik <mausamikarogalo1@gmail.com>
2022-10-06 15:16:52 +0530

    commit

---
+++ Javascript_basics/comments.js
@@ -0,0 +1,23 @@
+// COMMENTS IN JS 
+
+// some important points --> ; is necessary 
+
+var a = 10 ;
+var b = 20 ;
+
+// single line comment 
+
+/*
+multi line comment 
+here i will write a function that adds 2 numbers 
+*/
+
+function add(a, b)
+{
+    return a + b ;
+}
+
+
+res = add(a, b) 
+print(res)
+
