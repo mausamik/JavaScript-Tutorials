aafb1b3cb9750aaf50d1917e33517aa0a616666e
mausamik <mausamikarogalo1@gmail.com>
2022-10-07 12:34:16 +0530

    commit

---
+++ Javascript_Objects/strings.js
@@ -0,0 +1,36 @@
+/*
+
+2 ways to define strings 
+
+1. by string literal 
+2. by string objects 
+*/
+
+// 1. string literal 
+var str = "Mausami" 
+console.log(str)
+
+
+// 2. by string objects 
+var str1 = new String("String object")
+console.log(str1)
+
+/*
+Methods on strings 
+    charAt()
+    charCodeAt()
+    concat()
+    indexOf()
+    lastIndexOf()
+    search()
+    match()
+    replace()
+    substr() -- fetch part of string (start, end) position 
+    substring() -- fetch part of string on basis of specified index 
+    slice() -- same as substr . but +ve & -ve indexes as well 
+    toLowerCase()
+    toUpperCase()
+    toString()
+    split() -- split string into substring array -- returns new array 
+    trim() -- trim white spaces 
+*/
